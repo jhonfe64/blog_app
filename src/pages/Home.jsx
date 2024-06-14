659f7eb3c42c028539699e48a373a98d27fc006c
JOHN PEREZ <jhonfe649@gmail.comm>
2024-06-14 03:22:01 -0500

    git push origin master

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,6 +35,10 @@ function Home() {
   const loadMoreArticles = () => {
     setLimitArticles(limitArticles + 5);
   };
+  
+  const reloadF = ()=>{
+    window.location.reload()
+  }
 
   return (
     <>
@@ -50,7 +54,7 @@ function Home() {
           )}
         </div>
         <button
-          onClick={() => window.location.reload()}
+          onClick={reloadF}
           className="bg-slate-500 text-white px-4 py-2 mb-4 rounded-xl"
         >
           Reiniciar la busqeda
